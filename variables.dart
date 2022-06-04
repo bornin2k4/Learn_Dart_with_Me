c9de0c6aaf3545e509c42bbfb5adecc498188d0e
bornin2k4 <84788741+bornin2k4@users.noreply.github.com>
2022-06-04 08:48:22 +0530

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,44 @@
+main() {
+
+// This is single line comment
+/* Multiline
+ comment */
+ 
+// General
+
+var name="Thalha";
+var age=17;
+var pi=3.14;
+
+age = 18; // changeable - only to the same data type
+
+dynamic a="Thalha";
+dynamic b=17;
+dynamic pi=3.14 
+
+b = "hi"; // changeable - to any data type
+
+// specific
+
+int num1=5;
+double num2=5.0;
+string str="Thalha";
+
+num2 = 7; // changeable
+
+// const & final keywords
+// for making a constant variable where you can't change its value
+// unchangeable
+
+const var str1="Tha";
+final var str2="Po"; 
+
+// null declaration
+
+var height;
+print(height); // output: null
+
+height = "10cm";
+print(height) // output: 10cm
+
+}
