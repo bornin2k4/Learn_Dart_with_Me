352c2bd3c25b13253b0af88fea770f008f4bde4c
bornin2k4 <84788741+bornin2k4@users.noreply.github.com>
2022-06-04 13:15:44 +0530

    Create Lists.dart

---
+++ Lists.dart
@@ -0,0 +1,11 @@
+main() {
+
+// Lists in dart
+// They are similar to arrays...
+
+var list1 = [1,2,3];
+print(list1); // op: [1,2,3]
+
+// You can access list elements by;
+print(list1[0]); // op: 1
+}
