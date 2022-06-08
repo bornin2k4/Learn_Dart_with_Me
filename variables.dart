57fb2d006013a6a952b6c12b50001d80ff62e540
bornin2k4 <84788741+bornin2k4@users.noreply.github.com>
2022-06-08 11:07:50 +0530

    Update variables.dart

--- variables.dart
+++ variables.dart
@@ -1,57 +1,56 @@
 main() {
-
 // This is single line comment
 /* Multiline
  comment */
- 
+
 // General
 
-var name="Thalha";
-var age=17;
-var pi=3.14;
+  var name = "Thalha";
+  var age = 17;
+  var pi = 3.14;
 
-age = 18; // changeable - only to the same data type
+  age = 18; // changeable - only to the same data type
 
-dynamic a="Thalha";
-dynamic b=17;
-dynamic pi=3.14 
+  dynamic a = "Thalha";
+  dynamic b = 17;
+  dynamic pii = 3.14;
 
-b = "hi"; // changeable - to any data type
+  b = "hi"; // changeable - to any data type
 
 // specific
 
-int num1=5;
-double num2=5.0;
-String str="Thalha";
+  int num1 = 5;
+  double num2 = 5.0;
+  String str = "Thalha";
 
-num2 = 7; // changeable
+  num2 = 7; // changeable
 
 // const & final keywords
 // for making a constant variable where you can't change its value
 // unchangeable
 
-const var str1="Tha";
-final var str2="Po"; 
+  const str1 = "Tha";
+  final str2 = "Po";
 
 // null declaration
 
-var height;
-print(height); // output: null
+  var height;
+  print(height); // output: null
 
-height = "10cm";
-print(height) // output: 10cm
+  height = "10cm";
+  print(height); // output: 10cm
 
 //Boolean data type (true or false)
-bool a=true;
-
+  bool boolean = true;
+  print(boolean);
 
 // We will discuss lists,maps which are also the data types of dart...
 // In the next blog...
-
 }
 
 // Keyword used
 /*
+print
 var
 String
 int
